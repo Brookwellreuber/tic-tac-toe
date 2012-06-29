e3e7bc54117a48255e696184c7ef987441daefb2
David <davidbrookwell-reuber@David-Brookwell-Reubers-MacBook-Pro.local>
2012-06-29 15:15:54 -0600

    Initial Commit

---
+++ tic-tac-toe/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tic-tac-toe
+//
+//  Created by David Brookwell-Reuber on 12-06-29.
+//  Copyright (c) 2012 Grant MacEwan University. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
